3b5442f056617f0a10adfc842186f05114f7abac
Abel Gascia <abelgascia@gmail.com>
2021-03-28 00:47:44 -0300

    Debug

--- client/report.js
+++ client/report.js
@@ -348,7 +348,7 @@ export default (report_xml, report_csv) => {
             generateMarks(header_line + 3, '13.5%', MARKS_OFFSETS_TOP, 'top');
             generateMarks(header_line + 6, '79%', MARKS_OFFSETS_BOTTOM, 'bottom');
 
-            box_cont.append($(`<ddiv class="botones-cont hidden"></div>`).append(botones_cont));
+            box_cont.append($(`<ddiv class="botones-cont"></div>`).append(botones_cont));
         }
         
         createDiagram('bucal', 41);
